f2b00dc18be3dd93bf26a116348e6954f8172141
Arun-M-2000 <[arunm101220@gmail.com]>
2023-07-10 17:36:10 +0530

    web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect ,flash
 from flask_mysqldb import MySQL
 from flask import jsonify
 import MySQLdb
@@ -14,6 +14,8 @@ app.config["MYSQL_DB"] = "tata"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
+
+app.secret_key = "arunmsddhuhiuehikdnmszkhiuheifnkndiuhddfferrgtg"
 # Home page
 @app.route("/")
 def home():
@@ -21,132 +23,60 @@ def home():
     sql = "SELECT emp_id, name, salary FROM employees"
     con.execute(sql)
     result = con.fetchall()
-    return jsonify(result), 200
+    return render_template('home.html',data=result), 200
 
-#Add employees
-@app.route("/addemployee", methods=["GET","POST","DELETE","PUT","PATCH"])
+#Add Employeess
+@app.route("/addemployee", methods=["POST", "GET"])
 def addemployee():
     if request.method == "POST":
-            EMP_ID = request.form['id']
-            NAME = request.form['name']
-            SALARY = request.form['salary']
-            con = mysql.connection.cursor()
-            sql = "INSERT INTO employees (emp_id, name, salary) VALUES (%s, %s, %s)"
-            try:
-                con.execute(sql, [EMP_ID, NAME, SALARY])
-                mysql.connection.commit()
-                con.close()
-                return jsonify(response={
-                "Error_message":"",
-                "Status":"Success",
-                "Text":"Employee added successfully"
-        }),200
-            except MySQLdb.IntegrityError as IntEr:
-                    print("Error:", str(IntEr))
-                    return jsonify(response={
-                    "Error_message":"Duplicate entry for 'emp_id'",
-                    "Status":"Fail",
-                    "Text":"employee not added successfully"
-                     }),404
+        con = mysql.connection.cursor()
+        EMP_ID = request.form['id']
+        NAME = request.form['name']
+        SALARY = request.form['salary']
+        con.execute("INSERT INTO employees (emp_id, name, salary) VALUES (%s, %s, %s)", (EMP_ID, NAME, SALARY))
+        mysql.connection.commit()
+        con.close()
+        return redirect(url_for('home'))
     else:
-        return jsonify(response={
-             "Error_message":"method is not POST",
-             "Status":"Fail",
-             "Text":"Give the HTTP method as POST"
-        }),404
-            
+        return render_template("addemployee.html")
+
 #UPDTAE EMPLOYEES
-@app.route("/update_emp",methods=["GET","POST","DELETE","PUT","PATCH"])
+@app.route("/update_emp",methods=["GET","POST"])
 def update_emp():
-    con=mysql.connection.cursor()
-    EMP_ID = request.form.get('EMP_ID')
-    con.execute("SELECT emp_id FROM employees WHERE emp_id=%s",(EMP_ID,))
-    success=con.fetchone()
-    if success is not None:
-        if request.method=="POST":
-            EMP_ID = request.form.get('EMP_ID')
-            NAME = request.form.get('name')
-            SALARY = request.form.get('salary')
-            con = mysql.connection.cursor()
-            con.execute("UPDATE employees SET emp_id=%s, name=%s, salary=%s WHERE emp_id=%s", (EMP_ID, NAME, SALARY,EMP_ID))
-            mysql.connection.commit()
-            con.close()
-            return jsonify(response={
-             "Error_message":"",
-             "Status":"Success",
-             "Text":"update employee successfully"
-        }),200
-        else:
-             return jsonify(response={
-             "Error_message":"YOUR METHOD IS NOT POST",
-             "Status":"FAIL",
-             "Text":"CHANGE YOUR HTTP REQUEST AS POST"
-        }),404
-
+    if request.method=="POST":
+        EMP_ID = request.form.get('EMP_ID')
+        NAME = request.form.get('name')
+        SALARY = request.form.get('salary')
+        con = mysql.connection.cursor()
+        con.execute("UPDATE employees SET emp_id=%s, name=%s, salary=%s WHERE emp_id=%s", [EMP_ID, NAME, SALARY,EMP_ID])
+        mysql.connection.commit()
+        con.close()
+        return redirect(url_for('home'))
     else:
-        return jsonify(response={
-             "Error_message":"There is no employee in provided emp_id",
-             "Status":"Fail",
-             "Text":"Employee NOT FOUND"
-        }),404
+        return redirect(url_for('home'))
 #Get Employees
-@app.route("/get_emp/<string:id>",methods=["GET","POST","PUT","DELETE","PATCH"])
+@app.route("/get_emp/<string:id>",methods=["GET","POST"])
 def get_emp(id):
-    con = mysql.connection.cursor()
-    con.execute("SELECT emp_id FROM employees WHERE emp_id=%s",(id,))
-    success=con.fetchone()
-    if success is not None:
-        if request.method=="GET":
-            con=mysql.connection.cursor()
-            con.execute("SELECT * FROM employees WHERE emp_id = %s", (id,))
-            result=con.fetchone()
-            return jsonify(response={
-                "Error_message":"",
-                "Status":"Success",
-                "Text":"get employee successfully."
-            }),200
-        else:
-            return jsonify (response={
-                "Error_message":"YOUR HTTP REQUEST IS NOT GET",
-                "Status":"Fail",
-                "Text":"Use HTTP Request as GET"
-            }),404
+    if request.method=="GET":
+        con=mysql.connection.cursor()
+        con.execute("SELECT * FROM employees WHERE emp_id = %s", (id,))
+        result=con.fetchone()
+        return render_template("update_emp.html",data=result)
     else:
-        return jsonify(response={
-             "Error_message":"There is no employee in given emp_id",
-             "Status":"Fail",
-             "Text":"Employee not get"
-        }),404
-
+        return redirect(url_for('home'))
 #DELETE EMPLOYEES
-@app.route("/delete_emp/<string:id>", methods=["GET","POST","DELETE","PUT","PATCH"])
+@app.route("/delete_emp/<string:id>", methods=["GET","DELETE"])
 def delete_emp(id):
-    con=mysql.connection.cursor()
-    con.execute("SELECT emp_id FROM employees WHERE emp_id=%s",[id])
-    success=con.fetchone()
-    if success is not None:
-        if request.method=="DELETE":
-            con=mysql.connection.cursor()
-            con.execute("DELETE FROM employees WHERE emp_id=%s",[id])
-            mysql.connection.commit()
-            con.close()
-            return jsonify (response={    
-            "error_message":"",
-            "status":"success",
-            "text":"emoloyee deleted",
-             }),200
-        else:
-            return jsonify (response={
-                "Error_message":"YOUR HTTP REQUEST IS NOT DELETE",
-                "Status":"Fail",
-                "Text":"Use HTTP Request as DELETE"
-            }),404
+    if request.args.get("_method") == "DELETE":
+        con = mysql.connection.cursor()
+        con.execute("DELETE FROM employees WHERE emp_id=%s", [id])
+        mysql.connection.commit()
+        con.close()
+        flash("Employee deleted successfully")
+        return redirect(url_for('home')), 302
     else:
-        return jsonify (response={    
-        "error_message":"Employee id is not available ",
-        "status":"fail",
-        "text":"emoloyee not deleted",
-            }),404
+        return redirect(url_for('home')), 302
 
 if __name__ == '__main__':
     app.run(debug=True)
+
